21854fd149003d1518777521617be04cfc82eecf
RomainVoivenel <romain.voivenel@etu.univ-orleans.fr>
2025-01-11 10:49:56 +0100

    Added tokens for services in the database

--- data/creation.sql
+++ data/creation.sql
@@ -16,3 +16,17 @@ create table if not exists UTILISE(
     foreign key (id_service) references SERVICE(id_service),
     unique (id_ident, id_service)
 );
+
+create table if not exists TOKEN_GROUP(
+    id_token_group integer primary key autoincrement,
+    id_service int,
+    nom_groupe text(50),
+    foreign key (id_service) references SERVICE(id_service)
+);
+
+create table if not exists TOKEN(
+    id_token integer primary key autoincrement,
+    id_token_group int,
+    valeur_token text(100),
+    foreign key (id_token_group) references TOKEN_GROUP(id_token_group)
+);
