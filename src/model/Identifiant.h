d6e68281fd0959347e907266b7e7bee53730cfdf
RomainVoivenel <romain.voivenel@etu.univ-orleans.fr>
2025-01-12 21:35:20 +0100

    Added documentations

--- src/model/Identifiant.h
+++ src/model/Identifiant.h
@@ -15,19 +15,68 @@ namespace model {
 
     public:
         Identifiant() = delete;
+        /**
+         * Create a login with an email and a password
+         * @param email Email of the login
+         * @param password password of the login
+         */
         Identifiant(const std::string &email, const std::string &password);
+        /**
+         * Create a login with an email and a password
+         * @param email Email of the login
+         * @param password Password of the login
+         */
         Identifiant(std::string &&email, std::string &&password);
+        /**
+         * Create a login with an email and a password
+         * @param email Email of the login
+         * @param password Password of the login
+         */
         Identifiant(const std::string &email, std::string &&password);
+        /**
+         * Create a login with an email and a password
+         * @param email Email of the login
+         * @param password Password of the login
+         */
         Identifiant(std::string &&email, const std::string &password);
 
+        /**
+         * Get the email of the login
+         * @return Email of the login
+         */
         [[nodiscard]] std::string getEmail() const;
+        /**
+         * Get the password of the login
+         * @return Password of the login
+         */
         [[nodiscard]] std::string getPassword() const;
 
+        /**
+         * Set the email of the login
+         * @param email New email of the login
+         */
         void setEmail(const std::string &email);
+        /**
+         * Set the email of the login
+         * @param email New email of the login
+         */
         void setEmail(std::string &&email);
+        /**
+         * Set the password of the login
+         * @param password New password of the login
+         */
         void setPassword(const std::string &password);
+        /**
+         * Set the password of the login
+         * @param password New password of the login
+         */
         void setPassword(std::string &&password);
 
+        /**
+         * Compare two logins
+         * @param other Login to compare
+         * @return True if the two objects are the same
+         */
         bool operator==(const Identifiant &other) const;
     };
 
--- src/model/TokenGroup.h
+++ src/model/TokenGroup.h
@@ -16,20 +16,65 @@ namespace model {
         std::vector<Token> m_tokens;
 
     public:
+        /**
+         * Create a group of token with a name
+         * @param name Name of the group of tokens
+         */
         explicit TokenGroup(const std::string &name);
+        /**
+         * Create a group of token with a name
+         * @param name Name of the group of tokens
+         */
         explicit TokenGroup(std::string &&name);
 
+        /**
+         * Get the name of the group
+         * @return Name of the group of tokens
+         */
         [[nodiscard]] std::string getName() const;
+        /**
+         * Get the tokens in the group
+         * @return Tokens stocked in the group
+         */
         [[nodiscard]] std::vector<Token> getTokens() const;
 
+        /**
+         * Set the name of the group
+         * @param name New name for the group
+         */
         void setName(const std::string &name);
+        /**
+         * Set the name of the group
+         * @param name New name for the group
+         */
         void setName(std::string &&name);
 
+        /**
+         * Add a token in the group
+         * @param token Token added in the group
+         */
         void addToken(const Token &token);
+        /**
+         * Add a token in the group
+         * @param token Token added in the group
+         */
         void addToken(Token &&token);
+        /**
+         * Remove a specific token from the group
+         * @param token The token to remove
+         */
         void removeToken(const Token &token);
+        /**
+         * Remove a specific token from the group
+         * @param token The token to remove
+         */
         void removeToken(Token &&token);
 
+        /**
+         * Compare two groups of tokens
+         * @param token_group Group to compare
+         * @return True if the two objects are the same
+         */
         bool operator==(const TokenGroup &token_group) const;
     };
 } // model
