e9803f6844438ef89a334ba8234fb04f68e081c7
RomainVoivenel <voivenelromain@gmail.com>
2025-02-14 01:16:12 +0100

    keeping IGenericDao because the others are useless

--- src/impl/ServiceDaoImpl.h
+++ src/impl/ServiceDaoImpl.h
@@ -9,7 +9,7 @@
 
 namespace impl {
 
-    class ServiceDaoImpl final: public bd::IServiceDao {
+    class ServiceDaoImpl final: public bd::IGenericDAO<model::Service> {
         bd::Connector &m_connector;
 
     public:
@@ -18,8 +18,8 @@ namespace impl {
         ~ServiceDaoImpl() override = default;
 
         std::vector<model::Service> findAll() override;
-        std::vector<model::Service> findByName(std::string &&name) override;
-        std::vector<model::Service> findByName(const std::string &name) override;
+        std::vector<model::Service> findByName(std::string &&name);
+        std::vector<model::Service> findByName(const std::string &name);
         void insert(const model::Service &item) override;
         void remove(const model::Service &item) override;
     };
