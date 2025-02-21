63460d360c467982f90129afe7e9259b459a150b
RomainVoivenel <voivenelromain@gmail.com>
2025-02-21 18:18:26 +0100

    added tests for ServiceDaoImpl

--- tests/unitests/impl/ServiceDaoImpl.test.cpp
+++ tests/unitests/impl/ServiceDaoImpl.test.cpp
@@ -3,16 +3,39 @@
 //
 
 #include <catch2/catch_all.hpp>
+
+#include "ServiceDaoImpl.h"
 #include "impl/IdentifiantDaoImpl.h"
 
-TEST_CASE("Test the insert in the database", "[ServiceDaoImpl]") {
+TEST_CASE("Test the DAO of ServiceDaoImpl", "[servicedaoimpl]") {
+	impl::ServiceDaoImpl serviceDao {};
 
-}
+	std::vector all_before_insert {serviceDao.findAll()};
 
-TEST_CASE("Test the getters in the database", "[ServiceDaoImpl]") {
+	model::Service service {0, "//service1"};
+	serviceDao.insert(service);
+	serviceDao.insert(model::Service {0, "//service2"});
+	serviceDao.insert(model::Service {0, "/:service3"});
 
-}
+	std::vector all_after_insert {serviceDao.findAll()};
 
-TEST_CASE("Test the delete in the database", "[ServiceDaoImpl]") {
+	CHECK(serviceDao.findByName("/").size() == 3);
+	CHECK(serviceDao.findByName("//").size() == 2);
+	CHECK(serviceDao.findByName("/:").size() == 1);
 
+	CHECK((all_before_insert.size() + 3) == all_after_insert.size());
+
+	CHECK((all_after_insert.size() - all_before_insert.size()) == 3);
+	serviceDao.remove(all_after_insert[all_after_insert.size() - 1]);
+
+	all_after_insert = serviceDao.findAll();
+	CHECK((all_after_insert.size() - all_before_insert.size()) == 2);
+	serviceDao.remove(all_after_insert[all_after_insert.size() - 1]);
+
+	all_after_insert = serviceDao.findAll();
+	CHECK((all_after_insert.size() - all_before_insert.size()) == 1);
+	serviceDao.remove(all_after_insert[all_after_insert.size() - 1]);
+
+	all_after_insert = serviceDao.findAll();
+	CHECK((all_after_insert.size() - all_before_insert.size()) == 0);
 }
